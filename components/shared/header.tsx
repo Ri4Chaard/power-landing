9e030eb440ad3fdab336e6dfeaaa355b8db6cf24
Ri4Chaard <yarik271202@gmail.com>
2024-08-29 18:21:48 +0300

    Shadcn installed

---
+++ components/shared/header.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { cn } from "@/lib/utils";
+import { Container } from "./container";
+import Image from "next/image";
+import { ModeToggle } from "./mode-toggle";
+
+interface Props {
+    className?: string;
+}
+
+export const Header: React.FC<Props> = ({ className }) => {
+    return (
+        <header className={cn("", className)}>
+            <Container className="flex items-center justify-between py-8">
+                <div className="flex items-center gap-4">
+                    <Image
+                        src="/logo.png"
+                        alt="logo"
+                        width={50}
+                        height={50}
+                        className="rounded-full"
+                    />
+                    {/* <div className="w-[75px] h-[75px] flex items-center justify-center rounded-full select-none font-bold bg-black">
+                        <p className="text-2xl uppercase font-black select-none">
+                            {"<力/> "}
+                        </p>
+                    </div> */}
+
+                    <h1 className="text-2xl uppercase font-black select-none">
+                        Chikara IT
+                    </h1>
+                </div>
+                <div className="flex items-center gap-3">
+                    <ModeToggle />
+                </div>
+            </Container>
+        </header>
+    );
+};

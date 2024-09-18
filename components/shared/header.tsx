b14441f4b279a05c650f6f6dea01204fcdac67a3
Maksym Kondratov <geset300@gmail.com>
2024-09-18 17:37:24 +0200

    Новый коммит

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -25,7 +25,7 @@ export const Header: React.FC<Props> = ({ className }) => {
                     </div> */}
 
           <h1 className="text-3xl uppercase font-black select-none">
-            Chikara IT {"<力/> "}
+            Chikara IT круто {"<力/> "}
           </h1>
         </div>
       </Container>
